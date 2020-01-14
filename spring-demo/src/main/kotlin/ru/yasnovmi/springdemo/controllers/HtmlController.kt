3097292fb65581eae7522fa73ac3d86519016935
Mikhail Yasnov <yasnov.mi@gmail.com>
2020-01-14 20:09:27 +0300

    Update url validation

--- spring-demo/src/main/kotlin/ru/yasnovmi/springdemo/controllers/HtmlController.kt
+++ spring-demo/src/main/kotlin/ru/yasnovmi/springdemo/controllers/HtmlController.kt
@@ -1,13 +1,15 @@
 package ru.yasnovmi.springdemo.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import ru.yasnovmi.springdemo.service.Counter
+import java.net.MalformedURLException
 import java.net.URL
-
 
 @RestController
 class HtmlController {
@@ -20,13 +22,17 @@ class HtmlController {
         return "hello world!"
     }
 
+    @RequestMapping("error")
+    fun testError(): Void {
+        throw ResponseStatusException(HttpStatus.BAD_REQUEST, "hello error!")
+    }
+
     @RequestMapping("new", method = [RequestMethod.GET])
     fun newURL(
-            @RequestParam("url")
+            @RequestParam("url", required = true)
             url: String
     ): String {
-        val uri = URL("https://$url")
-        counter.addNew(uri.host.toLowerCase())
+        counter.addNew(parseUrl(url))
         return "ok"
     }
 
@@ -34,4 +40,18 @@ class HtmlController {
     fun getTop(): String {
         return counter.getTop().toString()
     }
+}
+
+fun parseUrl(url: String): String {
+    val u = try {
+        URL(url)
+    } catch (ex: MalformedURLException) {
+        URL("http://$url")
+    }
+
+    val pattern = "^[-a-zA-Z0-9+&@#/%?=~_|!:,;]+\\.[-a-zA-Z0-9+&@#/%?=~_|!:,.;]+\$".toRegex()
+    if (!u.host.matches(pattern)) {
+        throw ResponseStatusException(HttpStatus.BAD_REQUEST, "$url not correct URL")
+    }
+    return u.host.toLowerCase()
 }
