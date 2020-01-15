eed162b3279dd0920fb1172515fc0ea29ee3614e
Mikhail Yasnov <yasnov.mi@gmail.com>
2020-01-15 21:32:13 +0300

    Separate controllers

---
+++ spring-demo/src/main/kotlin/ru/yasnovmi/springdemo/controllers/DogImageController.kt
@@ -0,0 +1,24 @@
+package ru.yasnovmi.springdemo.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import ru.yasnovmi.springdemo.webclient.DogWebClient
+
+@RestController
+@RequestMapping("dog")
+class DogImageController {
+
+    @Autowired
+    lateinit var dogClient: DogWebClient
+
+    @RequestMapping("image", method = [RequestMethod.GET], produces = [MediaType.TEXT_HTML_VALUE])
+    fun getRandomDog(): Mono<String> {
+        return dogClient.getDogImage().map {
+            "<img src='${it.message}'/>"
+        }
+    }
+}
--- spring-demo/src/main/kotlin/ru/yasnovmi/springdemo/controllers/TopDomainController.kt
+++ spring-demo/src/main/kotlin/ru/yasnovmi/springdemo/controllers/TopDomainController.kt
@@ -1,36 +1,19 @@
-package ru.yasnovmi.springdemo.controller
+package ru.yasnovmi.springdemo.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
-import reactor.core.publisher.Mono
 import ru.yasnovmi.springdemo.dto.DomainDto
 import ru.yasnovmi.springdemo.service.Counter
 import ru.yasnovmi.springdemo.utils.parseUrl
-import ru.yasnovmi.springdemo.webclient.DogWebClient
 
 @RestController
-class HtmlController {
-
-    @Autowired
-    lateinit var dogClient: DogWebClient
+@RequestMapping("domain")
+class TopDomainController {
 
     @Autowired
     @Qualifier("dbDomainCounter")
     lateinit var counter: Counter
-
-    @RequestMapping("hello")
-    fun helloWorld(): String {
-        return "hello world!"
-    }
-
-    @RequestMapping("error")
-    fun testError(): Void {
-        throw ResponseStatusException(HttpStatus.BAD_REQUEST, "hello error!")
-    }
 
     @RequestMapping("new", method = [RequestMethod.GET])
     fun newParamUrl(
@@ -47,13 +30,6 @@ class HtmlController {
     ): String {
         counter.addNew(parseUrl(domain.domain))
         return "ok"
-    }
-
-    @RequestMapping("dog", method = [RequestMethod.GET], produces = [MediaType.TEXT_HTML_VALUE])
-    fun getRandomDog(): Mono<String> {
-        return dogClient.getDogImage().map {
-            "<img src='${it.message}'/>"
-        }
     }
 
     @RequestMapping("top", method = [RequestMethod.GET])
